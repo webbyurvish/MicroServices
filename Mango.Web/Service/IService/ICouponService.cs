d009827d473d16d359ceeccb103506d479c69a67
Urvish <webbyurvish@gmail.com>
2024-04-24 23:37:27 +0530

    Add project files.

---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(string id);
+        Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponsAsync(int id);
+    }
+}
